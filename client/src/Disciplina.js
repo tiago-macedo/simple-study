db51183df196e08c49ca247306eebe44eb8edb3c
Delpreti <rddesiqueira@gmail.com>
2021-06-06 18:19:26 -0300

    componentes de disciplina/grade

---
+++ client/src/Disciplina.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Disciplina(props){
+
+    return(
+        <div className="disciplina">
+            {/* organizar a partir do props.periodo */}
+            <p className="nome">{props.nome}</p>
+            <p className="codigo">{props.codigo}</p>
+            <p className="horario">{props.horario}</p>
+        </div>
+    )
+}
+
+export default Disciplina;
